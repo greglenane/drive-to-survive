807c1e46e887e361aba67db7e59cb4fea17b07d4
greglenane <greglenane@gmail.com>
2026-01-06 21:34:58 -0500

    updates for secret use and first script run test

--- drive-to-survive-etl.py
+++ drive-to-survive-etl.py
@@ -14,13 +14,19 @@ import yaml
 rnd = "24"
 year = "2025"
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 811a1e9 (updates for secret use and first script run test)
 
 s3_region = os.getenv('S3_REGION')
 s3_access_key_id = os.getenv('S3_ACCESS_KEY_ID')
 s3_secret_access_key = os.getenv('S3_SECRET_ACCESS_KEY')
 
+<<<<<<< HEAD
 =======
 >>>>>>> 08a2c1b (initial setup for gh actions with duckdb method)
+=======
+>>>>>>> 811a1e9 (updates for secret use and first script run test)
 gp_api_temp_path     = "s3://greglenane-drive-to-survive/api/gp/gp_api_temp.parquet"
 gp_api_main_path     = "s3://greglenane-drive-to-survive/api/gp/gp_api_main.parquet"
 gp_results_path      = "s3://greglenane-drive-to-survive/gp/gp_results.parquet"
@@ -29,6 +35,9 @@ sprint_api_main_path = "s3://greglenane-drive-to-survive/api/sprint/sprint_api_m
 sprint_results_path  = "s3://greglenane-drive-to-survive/sprint/sprint_results.parquet"
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 811a1e9 (updates for secret use and first script run test)
 # Enable S3 access
 con = duckdb.connect()
 con.execute("INSTALL httpfs;")
@@ -38,9 +47,12 @@ SET s3_region= '{s3_region}';
 SET s3_access_key_id= '{s3_access_key_id}';
 SET s3_secret_access_key= '{s3_secret_access_key}';
 """)
+<<<<<<< HEAD
 =======
 
 >>>>>>> 08a2c1b (initial setup for gh actions with duckdb method)
+=======
+>>>>>>> 811a1e9 (updates for secret use and first script run test)
 
 # functions
 # Robust Parser for DuckDB-stringified JSON
