12495c1af189fe6ebbe65022a9968ad2ddc7ab84
greglenane <greglenane@gmail.com>
2026-03-09 16:56:02 -0400

    add check for sprint results before performing qc

--- quality_checks/check-missing-results.py
+++ quality_checks/check-missing-results.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import duckdb
 import os
+import botocore
+import boto3
 
 # set variables
 s3_region = os.getenv('S3_REGION')
@@ -9,8 +11,6 @@ s3_secret_access_key = os.getenv('S3_SECRET_ACCESS_KEY')
 
 gp_results_path      = "s3://greglenane-drive-to-survive/gp/gp_results.parquet"
 sprint_results_path  = "s3://greglenane-drive-to-survive/sprint/sprint_results.parquet"
-
-print("Begin check-missing-results.py")
 
 # Enable S3 access
 con = duckdb.connect()
@@ -23,16 +23,31 @@ SET s3_access_key_id= '{s3_access_key_id}';
 SET s3_secret_access_key= '{s3_secret_access_key}';
 """)
 
+# s3 file check funciton
+def s3_file_exists(s3_path):
+    # parse the path we are checking a file in
+    s3_url = s3_path.replace("s3://", "").split("/", 1)
+    bucket = s3_url[0]
+    key = s3_url[1]
+    
+    try:
+        s3.head_object(Bucket=bucket, Key=key)
+        return True
+    except botocore.exceptions.ClientError:
+        return False
+
+print("Begin check-missing-results.py")
+
 # Load gp_results data
 gp_results = con.execute(f"SELECT * FROM read_parquet('{gp_results_path}')").df()
 
 # Check record count per round
 records_per_round = gp_results.groupby('round').size()
 
-if not (records_per_round == 20).all():
-    print("WARNING: Not all rounds have exactly 20 records")
+if not (records_per_round == 22).all():
+    print("WARNING: Not all rounds have exactly 22 records")
     for round_number, count in records_per_round.items():
-        if count != 20:
+        if count != 22:
             print(f"Round {round_number} has {count} results")
 
 # Check for gaps in round numbers
@@ -46,17 +61,27 @@ if missing_rounds:
 else:
     print("There are no missing rounds in GP results")
 
-# Load sprint_results data
-sprint_results = con.execute(f"SELECT * FROM read_parquet('{sprint_results_path}')").df()
+s3 = boto3.client(
+    's3',
+    region_name=s3_region,
+    aws_access_key_id=s3_access_key_id,
+    aws_secret_access_key=s3_secret_access_key
+)
 
-# check record count per sprint round
-sprint_records_per_round = sprint_results.groupby('round').size()
-print(f"Sprint rounds with results: {sorted(int(x) for x in sprint_records_per_round.index.tolist())}")
+if s3_file_exists(sprint_results_path):
+    # Load sprint_results data
+    sprint_results = con.execute(f"SELECT * FROM read_parquet('{sprint_results_path}')").df()
 
-if not (sprint_records_per_round == 20).all():
-    print("WARNING: Not all sprint rounds have exactly 20 records")
-    for round_number, count in sprint_records_per_round.items():
-        if count != 20:
-            print(f"Sprint Round {round_number} has {count} picks")
+    # check record count per sprint round
+    sprint_records_per_round = sprint_results.groupby('round').size()
+    print(f"Sprint rounds with results: {sorted(int(x) for x in sprint_records_per_round.index.tolist())}")
+
+    if not (sprint_records_per_round == 20).all():
+        print("WARNING: Not all sprint rounds have exactly 20 records")
+        for round_number, count in sprint_records_per_round.items():
+            if count != 20:
+                print(f"Sprint Round {round_number} has {count} picks")
+    else:
+        print("All sprint rounds have exactly 20 records")
 else:
-    print("All sprint rounds have exactly 20 records")
+    print("No sprint results file found at the specified S3 path")
